8916aae29d052b0606cae77c75ea144f94521061
mugane <muganes25@gmail.com>
2023-04-24 21:14:26 -0700

    0x4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,14 @@
 # Puppet manifest that configures ssh to disallow password authentication
-$config = file('/alx-system_engineering-devops/0x0B-ssh/2-ssh_config')
-file {'/etc/ssh/ssh_config':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0600',
-  content => $config
+include stdlib
+
+file_line { 'ssh_config_0':
+  ensure => absent,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
+}
+
+file_line { 'ssh_config_1':
+  ensure => absent,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school'
 }
