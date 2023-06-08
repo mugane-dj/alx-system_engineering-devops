dfa76de7942e0fec3f1e48c797cee535e55347b4
Samuel Mugane <muganes25@gmail.com>
2023-06-08 08:32:02 +0300

    update path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Apache returns 500 error due to bug in php module
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/.phpp/.php/ var/www/html/wp-settings.php',
+  command => 'sed -i s/.phpp/.php/ /var/www/html/wp-settings.php',
   path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin'
 }
