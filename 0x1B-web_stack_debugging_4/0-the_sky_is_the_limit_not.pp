7115d7ff24592e67a1f3f8da64b26d5cc171746e
Samuel Mugane <muganes25@gmail.com>
2023-06-13 13:20:19 +0300

    lint update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,30 @@
 # Tune nginx to handle concurrent requests
 
-exec { 'add-workers':
-  command => 'service nginx stop; sed -i "s/worker_processes 4/worker_processes 20/" /etc/nginx/nginx.conf',
+exec { 'stop-nginx':
+  command => 'service nginx stop',
   path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin'
+}
+
+exec { 'add-workers':
+  command => 'sed -i "s/worker_processes 4/worker_processes 20/" /etc/nginx/nginx.conf',
+  path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin',
+  require => Exec['stop-nginx']
 }
 
 exec { 'connections-config':
     command => 'sed -i "s/worker_connections 768/worker_connections 100/" /etc/nginx/nginx.conf',
-    path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin'
-}
-
-exec { 'multiaccept-config':
-    command => 'sed -i "s/# multi_accept on/multi_accept on/" /etc/nginx/nginx.conf',
-    path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin'
+    path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin',
+    require => Exec['add-workers']
 }
 
 exec { 'keepalive-config':
-    command => 'sed -i "/^http {/a \        keepalive_requests 10000;" /etc/nginx/nginx.conf; service nginx restart',
-    path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin'
+    command => 'sed -i "/^http {/a \        keepalive_requests 10000;" /etc/nginx/nginx.conf',
+    path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin',
+    require => Exec['connections-config']
+}
+
+exec { 'restart-nginx':
+  command => 'service nginx restart',
+  path    => 'usr/local/bin:usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/bin',
+  require => Exec['keepalive-config']
 }
